0f983fe7efd460dfac9bbdac0e26a0b688fb7fe8
DakshJoon <duxjoon@gmail.com>
2026-07-14 15:05:24 +0530

    i started leetcode

---
+++ src/lootcode1.java
@@ -0,0 +1,28 @@
+public class lootcode1 {
+    public static void main(String[] args) {
+        // today we are going to solve some problem in our vs code
+        
+        // problem 1672. richest customer wealth
+        int[][] accounts = {{2,2,3},
+                            {1,3,14}};
+        // this is a 2d array which have two customer as we can see
+        System.out.println(maximumWealth(accounts));
+    }
+    public static int maximumWealth(int[][] accounts){
+        // i = row, j = col
+        int maximum = 0;
+        for(int i = 0; i<accounts.length; i++){
+            int sum = 0;
+            // whem you start a new col take the sum for that row
+            for(int j = 0; j<accounts[i].length;j++){
+                sum += accounts[i][j];
+                // now we have sum of accounts of a person
+            }
+            // here we will check if the sum is greater then macimum or not
+            if (sum>maximum) {
+                maximum = sum;
+            }
+        } 
+        return maximum;
+    }
+}
