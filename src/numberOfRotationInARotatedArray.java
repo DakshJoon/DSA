941ed08beef2c9ef2101f25e614dcfaec20b1eee
DakshJoon <duxjoon@gmail.com>
2026-07-17 15:20:48 +0530

    added condition if the array is not rotated

--- src/numberOfRotationInARotatedArray.java
+++ src/numberOfRotationInARotatedArray.java
@@ -1,22 +1,25 @@
 public class numberOfRotationInARotatedArray {
     public static void main(String[] args) {
         // here we need to find the rotation of a rotated array
-        int[] arr = {15,18,2,3,6,12};
-        System.out.println(numberOfRotation(arr)); // this will give us the number of rotation
+        int[] arr = {1,2,3,4,5,6};
+        System.out.println(ans(arr)); // this will give us the number of rotation
+    }
+    public static int ans(int[] arr){
+        if (numberOfRotation(arr) == -1) {
+            return 0;
+        }
+        return numberOfRotation(arr);
     }
     public static int numberOfRotation(int[] arr){
-        return povit(arr)+1;
-    }
-    public static int povit(int[] arr){
         int start = 0;
         int end = arr.length-1;
         while(start<=end){
             int middle = start + (end - start)/2;
             if (middle<end&&arr[middle]>arr[middle+1]) {
-                return middle;
+                return middle+1;
             }
             if (middle>start && arr[middle]<arr[middle-1]) {
-                return middle-1;
+                return middle;
             }
             if(arr[middle]<=arr[start]){
                 end = middle -1;
