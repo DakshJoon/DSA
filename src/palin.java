548ded247fce871f84bd2f14714750a78c625045
DakshJoon <duxjoon@gmail.com>
2026-08-01 18:48:04 +0530

    learned about palindrome

---
+++ src/palin.java
@@ -0,0 +1,20 @@
+public class palin {
+    public static void main(String[] args){
+        String str = "abcdcba";
+        System.out.println(isPalindrome(str));
+    }
+    public static boolean isPalindrome(String str){
+        if(str == null || str.length() == 0){
+            return true;
+        }
+        str = str.toLowerCase();
+        for(int i = 0; i<str.length()/2; i++){
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-i-1);
+            if(start != end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
