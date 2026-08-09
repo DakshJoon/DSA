7ce3f0fcf7e30f8ad82106c81504001fac6348fb
DakshJoon <duxjoon@gmail.com>
2026-08-10 00:43:40 +0530

    learned about complex patterns

--- src/StarPattern1.java
+++ src/StarPattern1.java
@@ -10,6 +10,11 @@ public class StarPattern1 {
         pattern3(6);
         mergeNumber(6);
         pattern4(6);
+        spacePattern(6);
+        pattern5(6);
+        pattern6(6);
+        pattern7(6);
+        pattern8(6);
     }
     public static void pattern(int s) { 
         for(int i = 0; i<s; i++) { // i = row
@@ -74,11 +79,83 @@ public class StarPattern1 {
             System.out.println();
         }
     }
-    public static void pattern4(int s) {
+    public static void pattern4(int s) {  // this is the merged program for pattern and pattern1
         for(int i = 0; i<2*s;i++) {
             int totalcol = i > s ? 2*s-i : i;
             for(int j = 0; j<totalcol; j++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void spacePattern(int n) {
+        for(int i = 0; i<n; i++) {
+            int spaces = n-i;
+            for(int s = 0; s<spaces; s++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern5(int n) {
+        for(int i = 1; i <= 2*n - 1; i++) {
+            int totalCol = i > n ? 2*n - i : i;
+            int spaces = n - totalCol;
+            for(int s = 0; s < spaces; s++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < totalCol; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern6(int s){
+        for(int i = 0; i<= 2*s-1; i++){
+            int totalCol = i > s ? 2*s - i : i;
+            int star = s+1-totalCol;
+            for(int a = 0; a<star; a++) {
+                System.out.print("*");
+            }
+            for(int j = 0; j<totalCol; j++) {
+                System.out.print("  ");
+            } 
+            for(int d = 0; d<star; d++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern7(int s) {  
+        for(int i = 0; i<=s ; i++) {
+            int space = s - i;
+            for(int j = 0; j<=space; j++) {
+                System.out.print("  ");
+            }
+            for(int j = i; j >= 1; j--){
+                System.out.print(j+ " ");
+            }
+            for(int j = 2; j<=i; j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern8(int s){
+        for(int i = 0; i<=2*s-1; i++){
+            int col = i>s ? 2*s-i : i;
+            int space = s-col;
+            for(int j = 0; j<=space; j++ ){
+                System.out.print("  ");
+            }
+            for(int j = col; j>=1 ; j--){
+                System.out.print(j+" ");
+            }
+            for(int j = 2; j<=col; j++){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
