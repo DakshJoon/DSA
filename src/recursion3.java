1a6f13ccf0748a2378ecced00e674845df8f1811
DakshJoon <duxjoon@gmail.com>
2026-09-04 17:55:51 +0530

    did some recursion basic question

--- src/recursion3.java
+++ src/recursion3.java
@@ -14,7 +14,11 @@ public class recursion3 {
         System.out.println(binarySearch(ans, 4));
         System.out.println(findAllIndex(ans1, 2));
         System.out.println(findAllIndex(ans2, 4));
-        
+        System.out.println(findAllIndex1(ans2, 4));
+        int[] ans3 = {5,6,7,8,9,1,2,3,4};
+        System.out.println(rotatedBS(ans3, 7));
+        System.out.println(rotatedBS(ans3, 3));
+        System.out.println(rotatedBS(ans3, 10));
     }
     public static boolean sortedOrNot(int[] array){ // this method will check if the array is sorted or not 
         if(array.length < 2){
@@ -82,9 +86,49 @@ public class recursion3 {
         if(index == array.length){
             return list;
         }
-        else if(array[index] == target){
+        else if(array[index] == target) {
             list.add(index);
         }
-        return helperForAL(array, target, index+1, list);
+        return helperForAL(array, target, index+1, list); 
+    }
+    public static ArrayList<Integer> findAllIndex1(int[] array, int target){ // this method will do the same job but it will not take the ArrayList in the argument call
+        return helperFAI(array, target, 0);
+    }
+    public static ArrayList<Integer> helperFAI(int[] array, int target, int index){ // helper function for @findAllIndex1 
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(index == array.length){
+            return ans;
+        }
+        // this will contain answer for that function call only
+        else if(array[index] == target) {
+            ans.add(index);
+        }
+        ArrayList<Integer> ansFromBelowCalls = helperFAI(array, target, index+1);
+        ans.addAll(ansFromBelowCalls);
+        return ans;
+    }
+    public static int rotatedBS(int[] arr, int target){ // this is for searching an element in rotated Array using binary search and recursion
+        return helperForRBS(arr, target, 0, arr.length-1);
+    }
+    public static int helperForRBS(int[] arr, int target, int start, int end){
+        if(start > end){
+            return -1;
+        }
+        int middle = start + (end - start)/2;
+        if(arr[middle] == target){
+            return middle;
+        }
+        else if(arr[start]<=arr[middle]){ // it means that the first half is sorted
+            if(target >= arr[start] && target <= arr[middle]){
+                return helperForRBS(arr, target, start, middle -1);
+            }
+            else {
+                return helperForRBS(arr, target, middle + 1, end);
+            }
+        }
+        if(target >= arr[middle] && target <= arr[end] ){
+            return helperForRBS(arr, target, middle + 1, end);
+        }
+        return helperForRBS(arr, target, start, middle - 1); 
     }
 }
