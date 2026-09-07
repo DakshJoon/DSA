ebc2e911a92bd4476475e7a05bc7c87fd71c22d8
Daksh Joon <duxjoon@gmail.com>
2026-09-07 22:30:37 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (96.21%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,21 +1,21 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        if(nums == null || nums.length == 0){
+    public int search(int[] nums, int target) { // this is binary search using recursion
+        if(nums.length == 0){
             return -1;
         }
-
-        int start = 0;
-        int end = nums.length-1;
-        while(start<=end){
-            int middle = start + (end - start)/2;
-            if(nums[middle] == target){
-                return middle;
-            } else if(nums[middle]>target){
-                end = middle -1;
-            } else if(nums[middle]< target){
-                start = middle +1;
-            }
+        return helperForBS(nums, target, 0, nums.length-1);
+    }
+    public int helperForBS(int[] nums, int target, int start, int end) { // this is the helper of @binarSearch method
+        if (start > end) {
+            return -1;
         }
-        return -1;
+        int middle = start + (end-start)/2;
+        if(nums[middle] == target) {
+            return middle;
+        }
+        else if(nums[middle]>target) {
+            return helperForBS(nums, target, start, middle-1);
+        }
+        return helperForBS(nums, target, middle + 1, end);
     }
 }
