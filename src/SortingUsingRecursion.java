f27dcc5254314146fcb0bef04a50df0cb4da46db
DakshJoon <duxjoon@gmail.com>
2026-09-07 22:34:55 +0530

    bubble sort using recursion

--- src/SortingUsingRecursion.java
+++ src/SortingUsingRecursion.java
@@ -3,22 +3,26 @@
 */
 import java.util.*;
 public class SortingUsingRecursion {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] nums = {2,3,1,4,5,8,6};
         sort(nums, 0);
         System.out.println(Arrays.toString(nums));
+
+        int[] anotherArray = {7, 2, 9, 1, 5, 3};
+        bubbleSort(anotherArray);
+        System.out.println(Arrays.toString(anotherArray));
     }
-    public static void sort(int[] nums, int index){
+    public static void sort(int[] nums, int index) { // selection sorting 
 
         // let the index be the minimum number of index
         int min = index;
-        if(index == nums.length-1){
+        if(index < 0 || index == nums.length-1) {
             return;
         }
 
         // finding the index of minimum number
-        for(int i = index+1; i<nums.length; i++){
-            if(nums[i]<nums[min]){
+        for(int i = index+1; i<nums.length; i++) {
+            if(nums[i]<nums[min]) {
                 min = i;
             }
         }
@@ -30,5 +34,28 @@ public class SortingUsingRecursion {
 
         // sorting the rest array using recursion
         sort(nums, index+1);
+    }
+    public static void bubbleSort(int[] nums) { // bubble sort
+        bubbleSort(nums, nums.length);
+    }
+
+    private static void bubbleSort(int[] nums, int length) { // this is helper function for @bubbleSort
+        if(length <= 1) {
+            return;
+        }
+
+        boolean swapped = false;
+        for(int i = 0; i < length - 1; i++) {
+            if(nums[i] > nums[i + 1]) {
+                int temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        if(swapped) {
+            bubbleSort(nums, length - 1);
+        }
     }
 }
