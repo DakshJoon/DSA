e8d7b7588cbc3920387431bec6c73f98391bd608
Daksh Joon <duxjoon@gmail.com>
2026-09-18 15:44:35 +0530

    Time: 1 ms (87.16%) | Memory: 44.2 MB (57.06%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,26 +1,17 @@
 class Solution {
     public List<List<Integer>> subsets(int[] array) {
-        Arrays.sort(array);
-
-        List<List<Integer>> outer = new ArrayList<>();
+         List<List<Integer>> outer = new ArrayList<>();
         outer.add(new ArrayList<>());
 
-        int start = 0;
-        int end = 0;
-        for(int i = 0; i < array.length; i++ ){
-            start = 0;
-            if(i > 0 && array[i] == array[i - 1]){
-                start = end + 1;
-            }
-            end = outer.size() - 1;
+        for(int num : array){
             int n = outer.size();
 
-            for(int j = start; j< n ; j++){
-                List<Integer> internal = new ArrayList<>(outer.get(j));
-                internal.add(array[i]);
+            for(int i = 0; i< n ; i++){
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
                 outer.add(internal);
             }
         }
-        return outer; 
+        return outer;
     }
 }
