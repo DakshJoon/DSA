a198834402ffd026f72c47a7ad5a57de680b8757
DakshJoon <duxjoon@gmail.com>
2026-09-22 22:04:26 +0530

    Maze question using ArrayList

--- src/Maze.java
+++ src/Maze.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args){
-        System.out.println(maze(4,4));
+        System.out.println(maze(3,3));
         mazePath(3, 3);
+
+        System.out.println();
+        System.out.println(mazePath1(3,3));
     }
 
 
@@ -33,4 +38,26 @@ public class Maze {
             mazePath(r, c-1 , path + "D");
         }
     }
+
+    public static ArrayList<String> mazePath1(int r , int c){
+        return mazePath1(r, c, "");
+    }
+    private static ArrayList<String> mazePath1(int r , int c, String path){
+        if(c == 1 && r == 1){
+            ArrayList<String> ans = new ArrayList<>();
+            ans.add(path);
+            return ans;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        if(r > 1){
+            ans.addAll(mazePath1(r - 1, c , path + "R"));
+        }
+
+        if(c > 1){
+            ans.addAll(mazePath1(r, c-1 , path + "D"));
+        }
+
+        return ans;
+        
+    } 
 }
